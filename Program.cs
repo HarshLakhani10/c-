df9b844881805cf56a711f512a67a0b55fc83590
harsh lakhani <harshlakhanip@gmail.com>
2025-03-18 21:47:24 +0530

    learn static and instance member of a class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Start
+{
+    public static void Main(string[] args)
+    {
+        #region static_and_instance_members
+
+        // Student S1 = new Student();
+        // S1.Name = "Harsh";
+        // System.Console.WriteLine(S1.Name);
+        // S1.ShowName();
+        // System.Console.WriteLine(Student.School);
+        // Student.ShowSchool();
+        // System.Console.WriteLine("--------------------------------------");
+        // Student S2 = new Student();
+        // S2.Name = "Nevil";
+        // System.Console.WriteLine(S2.Name);
+        // S2.ShowName();
+        // System.Console.WriteLine(Student.School);
+
+
+        //static member of a class only take one space throughout the program and access and modified from that location
+        // while the instance/object of a class has fresh new copy of every instance member of a class so it will multiple space as more instance/object will br made.
+
+
+        #endregion
+    }
+}
